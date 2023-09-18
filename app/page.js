b83ceb916ff8681718c1df0716825c74423fe997
Youbraj Singh <blazeplays2003@gmail.com>
2023-09-18 22:49:28 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,62 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const page = () => {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [mainTask, setmainTask] = useState([]);
+
+  const submitHandle = () => {
+    setmainTask([...mainTask, { title, desc }]);
+    settitle("");
+    setdesc("");
+    console.log(mainTask);
+  }
+
+  const deltask = (i)=>{
+    const cpymain = [...mainTask];
+    cpymain.splice(i,1);
+    setmainTask(cpymain);
+  }
+
+  let render = <h2 style={{padding: 1 + 'rem'}}>no task availabe</h2>
+
+  if (mainTask.length > 0) {
+    render = mainTask.map((t, i) => {
+      return <li key={i}>
+        <div className='task'>
+          <h5>{t.title}</h5>
+          <h6>{t.desc}</h6>
+        <button onClick={()=>{
+          deltask(i)
+        }} className='del'>delete</button>
         </div>
+      </li>
+    })
+  }
+
+  return (
+    <>
+      <h1>Todo App</h1>
+      <div className="inputs">
+        <input type="text" placeholder='Enter the title' value={title} onChange={(e) => {
+          settitle(e.target.value);
+        }} />
+        <input type="text" placeholder='Enter the description' value={desc} onChange={(e) => {
+          setdesc(e.target.value);
+        }} />
+        <button onClick={submitHandle} className='addtask'>Add Task</button>
+      </div>
+      <hr />
+      <div className="tasks">
+        <ul>
+          {render}
+        </ul>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
+
+export default page
